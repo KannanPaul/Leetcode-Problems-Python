96d3f594d462e61c6db080bb20566d02cb391299
Kannan Paulvannan <41234212+KannanPaul@users.noreply.github.com>
2022-03-03 15:10:46 +0530

    Update Reverse String (Two Pointers).py

--- Reverse String (Two Pointers).py
+++ Reverse String (Two Pointers).py
@@ -1,1 +1,24 @@
+'''Write a function that reverses a string. The input string is given as an array of characters s.
 
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+Example 1:
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Example 2:
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+'''
+
+#Solution:
+s=["h","e","l","l","o"]
+l=0
+r=len(s)-1   
+while l<=r:
+  s[l],s[r]=s[r],s[l]
+  l+=1
+  r-=1
+print(s)
