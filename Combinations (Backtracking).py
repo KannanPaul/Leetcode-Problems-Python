42ebd8eda0ed0cd05b682873096c8c5ec4edd6ef
Kannan Paulvannan <41234212+KannanPaul@users.noreply.github.com>
2022-04-08 13:15:04 +0530

    Update Combinations (Backtracking).py

--- Combinations (Backtracking).py
+++ Combinations (Backtracking).py
@@ -21,3 +21,17 @@ Output: [[1]]
 
 #Solution - 1 : 
 # Time complexity -
+
+def combine(self, n: int, k: int) -> List[List[int]]:
+        sol=[]
+        def backtrack(remain,comb,nex):
+            if remain==0:
+                sol.append(comb.copy())
+            else:
+                for i in range(nex,n+1):
+                    comb.append(i)
+                    backtrack(remain-1,comb,i+1)
+                    comb.pop()
+                    
+        backtrack(k,[],1)
+        return sol
