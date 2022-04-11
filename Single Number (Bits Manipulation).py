728ac71637f0c26db0622e05a16ff4e1ea31e2bd
Kannan Paulvannan <41234212+KannanPaul@users.noreply.github.com>
2022-04-11 11:58:29 +0530

    Update Single Number (Bits Manipulation).py

--- Single Number (Bits Manipulation).py
+++ Single Number (Bits Manipulation).py
@@ -1,1 +1,26 @@
+'''
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
 
+Example 1:
+Input: nums = [2,2,1]
+Output: 1
+
+Example 2:
+Input: nums = [4,1,2,1,2]
+Output: 4
+
+Example 3:
+Input: nums = [1]
+Output: 1
+'''
+
+#Solution 1 : Bits Manipulation
+# Time complexity - O(n)
+# Space complexity - O(1)
+
+def singleNumber(self, nums: List[int]) -> int:
+        s=0
+        for i in nums:
+            s^=i
+        return s
