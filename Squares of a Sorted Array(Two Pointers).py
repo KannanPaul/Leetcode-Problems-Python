61a089437e50d72fcbfbfe9f1a97dacef9972967
Kannan Paulvannan <41234212+KannanPaul@users.noreply.github.com>
2022-05-22 21:33:25 +0530

    Update Squares of a Sorted Array(Two Pointers).py

--- Squares of a Sorted Array(Two Pointers).py
+++ Squares of a Sorted Array(Two Pointers).py
@@ -11,7 +11,7 @@ Example 2:
 Input: nums = [-7,-3,2,3,11]
 Output: [4,9,9,49,121]
 '''
-#Solution -1 :(Two Pointers - O(n) time complexity and O(1) auxillary space)
+#Solution -1 :(Two Pointers - O(n) time complexity and O(n) auxillary space)
 
 answer = [0] * len(nums)
 l, r = 0, len(nums) - 1
@@ -25,3 +25,23 @@ while l <= r:
     r -= 1
   
 print(answer)
+
+
+#solution 2: (Two Pointers - O(n) time complexity and O(n) auxillary space)
+
+def sortedSquares(self, nums: List[int]) -> List[int]:
+        l=0
+        h=len(nums)-1
+        r=[0]*len(nums)
+        n=h
+        while l<=h:
+            ls= nums[l]**2
+            hs= nums[h]**2
+            if ls <=hs:
+                r[n]=hs
+                h=h-1
+            elif ls >= hs:
+                r[n]=ls
+                l=l+1
+            n=n-1
+        return r 
