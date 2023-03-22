f734843485d0981bfd3da4cbc06914891781b53b
Kannan Paulvannan <41234212+KannanPaul@users.noreply.github.com>
2023-03-22 17:39:24 +0530

    Update 3Sum (Two Pointers).py

--- 3Sum (Two Pointers).py
+++ 3Sum (Two Pointers).py
@@ -40,3 +40,27 @@ Constraints:
 # Solution 1:
 # Time Complexity : O( N*N)
 # Space Complexity : O(M) where M is the number of solutions
+
+def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res=[]
+        nums.sort()
+
+        for i in range(len(nums)-2):
+            if i==0 or (i>0 and nums[i] != nums[i-1]):
+                low = i + 1
+                high = len(nums)-1
+                sum = 0 - nums[i]
+            while low < high:
+                if nums[low] + nums[high] == sum:
+                    res.append([nums[i], nums[low], nums[high]])
+                    while low < high and nums[low] == nums[low + 1]:
+                            low += 1
+                    while low < high and nums[high] == nums[high - 1]:
+                            high -= 1
+                    low += 1
+                    high -= 1
+                elif nums[low]+ nums[high] < sum:
+                    low += 1
+                elif nums[low]+ nums[high] > sum:
+                    high -= 1
+        return res
