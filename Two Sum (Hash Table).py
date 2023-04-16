ab8f3d1a89589515a0f9c8fc5aebf629c9f7fb56
Kannan Paulvannan <41234212+KannanPaul@users.noreply.github.com>
2023-04-16 11:07:30 +0530

    Update Two Sum (Hash Table).py

--- Two Sum (Hash Table).py
+++ Two Sum (Hash Table).py
@@ -40,6 +40,12 @@ Algorithm
 
 It turns out we can do it in one-pass. While we are iterating and inserting elements into the hash table, we also look back to check if current element's complement already exists in the hash table. If it exists, we have found a solution and return the indices immediately.
 
+Complexity Analysis
+
+    Time complexity: O(n). We traverse the list containing nnn elements only once. Each lookup in the table costs only O(1)O(1)O(1) time.
+
+    Space complexity: O(n). The extra space required depends on the number of items stored in the hash table, which stores at most nnn elements
+    
 '''
 
 def twoSum(self, nums: List[int], target: int) -> List[int]:
@@ -49,3 +55,5 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
             if complement in hashmap:
                 return [i, hashmap[complement]]
             hashmap[nums[i]] = i
+
+        
