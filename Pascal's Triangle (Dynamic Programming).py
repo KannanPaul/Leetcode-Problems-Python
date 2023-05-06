5c1e3e8e4cf1b8db56937c64170b58f048eebcac
Kannan Paulvannan <41234212+KannanPaul@users.noreply.github.com>
2023-05-06 21:02:57 +0530

    Update Pascal's Triangle (Dynamic Programming).py

--- Pascal's Triangle (Dynamic Programming).py
+++ Pascal's Triangle (Dynamic Programming).py
@@ -27,13 +27,12 @@ Constraints:
 # Solution 1 : 
 
 def generate(self, numRows: int) -> List[List[int]]:
-        res= [[1]]
-
-        for row in range(1,numRows):
-            temp = [1]
+        res = [[1]]
+        for i in range(1,numRows):
+            temp = []
             for i in range(len(res[-1])-1):
-                temp = temp + [res[-1][i]+res[-1][i+1]]
-            temp = temp + [1]
+                temp = temp + [res[-1][i] + res[-1][i+1]]
+            temp = [1] + temp + [1]
             res = res + [temp]
         
         return res
