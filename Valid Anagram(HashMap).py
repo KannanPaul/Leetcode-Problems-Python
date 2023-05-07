f70b0955628dd91844151803bd7c38f7af71a4b8
Kannan Paulvannan <41234212+KannanPaul@users.noreply.github.com>
2023-05-07 15:48:25 +0530

    Update Valid Anagram(HashMap).py

--- Valid Anagram(HashMap).py
+++ Valid Anagram(HashMap).py
@@ -43,3 +43,24 @@ def isAnagram(self, s: str, t: str) -> bool:
             return True
         else:
             return False
+
+# Solution 2 :
+
+def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        sdict = {}
+        for str in s:
+            if str in sdict:
+                sdict[str] += 1
+            else:
+                sdict[str] = 1
+        
+        for str in t:
+            if not str in sdict:
+                return False
+            elif sdict[str] == 0:
+                return False
+            sdict[str] -= 1
+        
+        return True
