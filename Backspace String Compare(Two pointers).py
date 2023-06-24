7b214c5bc2d0ef6fb96872561936ba0fd542b291
Kannan Paulvannan <41234212+KannanPaul@users.noreply.github.com>
2023-06-24 14:47:35 +0530

    Create Backspace String Compare(Two pointers).py

---
+++ Backspace String Compare(Two pointers).py
@@ -0,0 +1,88 @@
+'''
+Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+ 
+
+Example 1:
+
+Input: s = "ab#c", t = "ad#c"
+Output: true
+Explanation: Both s and t become "ac".
+
+Example 2:
+
+Input: s = "ab##", t = "c#d#"
+Output: true
+Explanation: Both s and t become "".
+
+Example 3:
+
+Input: s = "a#c", t = "b"
+Output: false
+Explanation: s becomes "c" while t becomes "b".
+
+ 
+
+Constraints:
+
+    1 <= s.length, t.length <= 200
+    s and t only contain lowercase letters and '#' characters.
+'''
+
+# Solution 1 : (Brute force solution)
+
+# Time complexity: O(S+T)
+# Space complexity: O(S+T)
+# where S & T - lenth of given strings S, T
+
+def backspaceCompare(self, S: str, T: str) -> bool:
+        def fn(a):
+            b=''
+            for i in range(0,len(a)):
+                if a[i] != '#':
+                    b=b+a[i]
+                else:
+                    b=b[:-1]
+            return b
+
+        c=fn(S)
+        d=fn(T)
+        if c==d:
+            return True
+        else:
+            return False
+
+# Solution 2: (Brute force solution)
+def backspaceCompare(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        sL = list(s)
+        tL = list(t)
+
+        sStack = []
+        tStack = []
+
+        index = 0
+        while index < len(sL) or index < len(tL):
+            sCurr = sL[index] if index < len(sL) else ""
+            tCurr = tL[index] if index < len(tL) else ""
+
+            if sCurr == "#":
+                if len(sStack) != 0: sStack.pop()
+            else:
+                sStack.append(sCurr)
+            if tCurr == "#":
+                if len(tStack) != 0: tStack.pop()
+            else:
+                tStack.append(tCurr)
+            index += 1
+        return "".join(sStack) == "".join(tStack)
+
+# Solution 3 : (Optimal solution using Two Pointers)
+        
+
