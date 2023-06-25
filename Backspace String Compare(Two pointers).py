9b500bfee2963740a98c9c7f827a231e5277349d
Kannan Paulvannan <41234212+KannanPaul@users.noreply.github.com>
2023-06-25 17:46:59 +0530

    Update Backspace String Compare(Two pointers).py

--- Backspace String Compare(Two pointers).py
+++ Backspace String Compare(Two pointers).py
@@ -84,5 +84,43 @@ def backspaceCompare(self, s, t):
         return "".join(sStack) == "".join(tStack)
 
 # Solution 3 : (Optimal solution using Two Pointers)
-        
+# Time complexity: O(s+t)
+# Space complexity: O(1)
+# where S & T - lenth of given strings S, T
+
+  def backspaceCompare(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+    
+        p1 = len(s)-1   
+        p2 = len(t)-1   
+        while p1 >=0 or p2 >= 0:
+            if (p1 >= 0 and s[p1] == '#') or (p2 >= 0 and t[p2] =='#'):
+                if s[p1] =='#':
+                    backcount = 2
+                    while backcount > 0:
+                        p1 -=1   
+                        backcount -= 1   
+                        if p1 >=0 and s[p1] == '#':
+                            backcount += 2
+                if t[p2] =='#':
+                    backcount = 2 
+                    while backcount > 0:
+                        p2 -=1
+                        backcount -= 1
+                        if p2 >= 0 and t[p2] =='#':
+                            backcount +=2
+            else:
+                if p1 <0 or p2 <0:
+                    return False
+                elif s[p1] != t[p2]:
+                    return False
+                else:
+                    p1 -= 1
+                    p2 -= 1
+        return True
+
 
