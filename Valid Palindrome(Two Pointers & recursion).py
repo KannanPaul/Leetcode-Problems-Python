a510d0895b8a19df574218f5759170cbe90225e9
Kannan Paulvannan <41234212+KannanPaul@users.noreply.github.com>
2023-12-30 20:31:10 +0530

    Update Valid Palindrome(Two Pointers & recursion).py

--- Valid Palindrome(Two Pointers & recursion).py
+++ Valid Palindrome(Two Pointers & recursion).py
@@ -1,4 +1,4 @@
-```
+'''
 A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
 
 Given a string s, return true if it is a palindrome, or false otherwise.
@@ -35,3 +35,22 @@ def isPalindrome(self, s: str) -> bool:
             return True
         else:
             return False
+
+# Solution 2: (Two Pointers)
+
+def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            if s[l].isalnum() == False:
+                l+= 1
+            elif s[r].isalnum() == False:
+                r -= 1
+            elif s[l].lower() != s[r].lower():
+                return False
+            else:
+                l += 1
+                r -= 1
+        return True
+
+
