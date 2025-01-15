3e0dc2f2f3143cb3dad13316b0679a707c093c2d
Kannan Paulvannan <41234212+KannanPaul@users.noreply.github.com>
2025-01-15 20:11:29 +0530

    Update Majority Element ( Moore’s Voting Algorithm\Hash table).py

--- Majority Element ( Moore’s Voting Algorithm\Hash table).py
+++ Majority Element ( Moore’s Voting Algorithm\Hash table).py
@@ -27,8 +27,7 @@ def majorityElement(self, nums: List[int]) -> int:
         for num in nums:
             if count == 0:
                 candidate = num
-            
-            if num == candidate:
+            elif num == candidate:
                 count += 1
             else:
                 count -= 1
