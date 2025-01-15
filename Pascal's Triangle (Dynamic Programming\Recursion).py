ad2a78172923e25c0a7f4e77b274e677e7f58e4f
Kannan Paulvannan <41234212+KannanPaul@users.noreply.github.com>
2025-01-15 17:55:00 +0530

    Update and rename Pascal's Triangle (Dynamic Programming).py to Pascal's Triangle (Dynamic Programming\Recursion).py

--- Pascal's Triangle (Dynamic Programming\Recursion).py
+++ Pascal's Triangle (Dynamic Programming\Recursion).py
@@ -50,3 +50,20 @@ def generate(self, numRows: int) -> List[List[int]]:
             res[-1] += [res[i-1][-1]]
         
         return res
+
+Solution 3: Recursion
+
+ def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 0:
+            return []
+        if numRows == 1:
+            return [[1]]
+        
+        prevRows = self.generate(numRows - 1)
+        newRow = [1] * numRows
+        
+        for i in range(1, numRows - 1):
+            newRow[i] = prevRows[-1][i - 1] + prevRows[-1][i]
+        
+        prevRows.append(newRow)
+        return prevRows
