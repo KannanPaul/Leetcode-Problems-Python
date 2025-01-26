c6e1a446a180ceee186a66d4a8656489e400fdff
Kannan Paulvannan <41234212+KannanPaul@users.noreply.github.com>
2025-01-26 21:51:43 +0530

    Update Merge Sorted Array (Two Pointers).py

--- Merge Sorted Array (Two Pointers).py
+++ Merge Sorted Array (Two Pointers).py
@@ -61,3 +61,41 @@ def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
             else:
                 nums1[m+n-1] = nums2[n-1]
                 n = n-1
+
+Variation 2:
+Given two non-decreasing sorted arrays, ‘A’ and ‘B’, having ‘N’ and ‘M’ elements, respectively.
+You must merge these arrays, ‘A’ and ‘B’, into a sorted array without using extra space. Of all the 'N + M' sorted elements, array 'A' should contain the first 'N' elements, and array 'B' should have the last 'M' elements.
+
+Note:
+You must perform the merge operation in place and must not allocate any extra space to merge the two arrays.
+
+For example:
+When ‘N’ = 4, ‘A’ = {1, 4, 5, 7} and ‘M’ = 3, ‘B’ = {2, 3, 6}. 
+We can merge these two arrays into {1, 2, 3, 4, 5, 6, 7} (The elements of ‘A’ are {1, 2, 3, 4} ).
+Hence, the answer is {1, 2, 3, 4, 5, 6, 7}.
+
+Detailed explanation ( Input/output format, Notes, Images )
+Sample Input 1:
+
+3 4
+1 8 8
+2 3 4 5
+
+Sample Output 1:
+1 2 3 4 5 8 8
+
+Explanation Of Sample Input 1:
+We have ‘A’ = {1, 8, 8} and ‘B’ = {2, 3, 4, 5}. 
+Merging the two arrays results in {1, 2, 3, 4, 5, 8, 8}.
+Hence the answer is {1, 2, 3, 4, 5, 8, 8}, where ‘A’ contains {1, 2, 3} and ‘B’ contains {4, 5, 8, 8}.
+
+Sample Input 2:
+4 5
+1 1 1 1 
+2 2 3 3 5
+
+Sample Output 2:
+1 1 1 1 2 2 3 3 5
+
+Solution Reference: https://www.geeksforgeeks.org/merge-two-sorted-arrays-o1-extra-space/
+
